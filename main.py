fccbb9d3b4a08e70b428f059503e4d908394627a
Flavian Anselmo <>
2021-05-23 09:52:38 +0000

    final

--- main.py
+++ main.py
@@ -8,18 +8,44 @@ A = [10, 5, 6, 9] reverseArray(A) A // [9, 6, 5, 10]
 Share the Github link to your solution.
 """
 def reverseArray(A):
-  for i in range(len(A)):
-    #taje the last item
-    #take the firstitem 
-    #swap
-    """
-    if the lenth of  the 
-    array is even the Prog'
-    will swap length/2
-    if odd it will swap length//2
-    """
-    
+  
+  #take the last item
+  #take the firstitem 
+  #swap
+  """
+  if the lenth of  the 
+  array is even the Prog'
+  will swap length/2
+  if odd it will swap length//2
+  """
+  #CHECK THE LEN DIVIDE BY 2
+  length=len(A)//2
+  rev=-1
+  revlen=-(length)
+  """
+  revlen is the length 
+  that is used to accessed
+  the items from the end of the list
+
+  swap the last item with the first 
+  item ,then move to the second item 
+  with the second last item 
+
+  the time complexity is O(n)
+  """
+  for i in range(length):
+    if i!=0:  
+      #if i is not 0 rev in incresed 
+      rev=rev-1#since it is a negative value
+    #make a swap   
+    A[i],A[rev]=A[rev],A[i]
+    #if we reach the last item 
+    #break from the loop
+    #rev is our counter from the last item 
+    if rev==revlen:
+      break#move out of the loop 
+  print(A)#print the reversed array 
 
 
-A=[10,5,6,9]
+A=[10,5,6,9,7]
 reverseArray(A)
