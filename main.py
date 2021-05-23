ec63a1ff85699b1be6e4b14247a239bcdd20af11
Flavian Anselmo <>
2021-05-23 08:19:19 +0000

    added comments

---
+++ main.py
@@ -0,0 +1,25 @@
+"""
+1. Reverse Array In-place
+Write a function reverseArray(A) that takes in an array A and reverses it, without using another array or collection data structure; in-place.
+
+Example:
+
+A = [10, 5, 6, 9] reverseArray(A) A // [9, 6, 5, 10]
+Share the Github link to your solution.
+"""
+def reverseArray(A):
+  for i in range(len(A)):
+    #taje the last item
+    #take the firstitem 
+    #swap
+    """
+    if the lenth of  the 
+    array is even the Prog'
+    will swap length/2
+    if odd it will swap length//2
+    """
+    
+
+
+A=[10,5,6,9]
+reverseArray(A)
